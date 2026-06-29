27b0398d3597bd2e9bfa9e4f458c3f742b022b6d
Tatiana Belikova <belikovaat@yandex.ru>
2026-06-29 17:14:19 +0300

    Третья попытка отправки работы

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,21 +1,28 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
+    // заполняем dropdown'ы
+    Object.keys(indexes).forEach((key) => {
+        const select = elements[key];
 
-    Object.keys(indexes).forEach((elementName) => {
-        elements[elementName].append(
-            ...Object.values(indexes[elementName]).map(name => new Option(name, name))
+        if (!select) return;
+
+        select.append(
+            ...Object.values(indexes[key]).map(name => new Option(name, name))
         );
     });
 
     return (data, state, action) => {
-
+        // очистка фильтра
         if (action && action.name === 'clear') {
             const input = action.parentElement.querySelector('input');
-            input.value = '';
-            state[action.dataset.field] = '';
+
+            if (input) input.value = '';
+
+            const field = action.dataset.field;
+            if (field) state[field] = '';
         }
 
         return data.filter(row => compare(row, state));
